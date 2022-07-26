af7c8f1499d0ea8f89527e9fd7a416af7df84b6d
tahmid <tahmid@tahmid.tahmid>
2022-07-27 01:09:35 +0600

    login page

--- user_login.php
+++ user_login.php
@@ -4,32 +4,22 @@
         <?php include 'menu.php';?>
     </nav> 
     <head>
-        <link href = 'CSS/login.css' rel='stylesheet'>
+         <link href = 'CSS/login.css' rel='stylesheet'>
     </head>
     <body>
         <?php include 'footer.php';?>
-        <section class = 'loginPanel'>
+         <section class = 'loginPanel'>
             <h1>Login To Your Account</h1>
             <div class = 'form' >
-                <form>
-                    <label for = 'username'> Enter Username</label><br>
-                    <input type = 'text' id = 'username' name = 'username' required ><br>
-                    <label for = 'password'>Enter Password</label><br>
-                    <input type = 'text' id = 'password' name = 'password' required ><br>
-                    <input type = 'submit' id = 'password' value = 'Login' >
-                </form>
+            <form>
+                <h4> Enter Username</h4>
+                <input class = "textField" type = 'text' id = 'username' name = 'username' required ><br>
+                 <h4> Enter Password</h4>
+                <input class = "textField"  type = 'text' id = 'password' name = 'password' required ><br>
+                <input class = 'loginButton' type = 'submit' id = 'submit' value = 'Login' >
+            </form>
          </div>
             <h3>Login Usign Accounts</h3>
         </section>
     </body>
 </html>
-
-
-<!-- 
-<form>
-                <label for = 'username'> Enter Username</label>
-                <input type = 'text' id = 'username' name = 'username' required ><br>
-                <label for = 'password'>Enter Password</label>
-                <input type = 'text' id = 'password' name = 'password' required >
-            </form>
- -->
--- user_register.php
+++ user_register.php
@@ -3,11 +3,12 @@
     <nav>
         <?php include 'menu.php';?>
     </nav> 
-    
+    <head>
+         <link href = 'CSS/login.css' rel='stylesheet'>
+    </head>
     <body>
         <?php include 'footer.php';?>
-        <div>
-            <h1>this is register page</h1>
-         </div>
+         <h1>Registration Page<h1>
+          
     </body>
 </html>
