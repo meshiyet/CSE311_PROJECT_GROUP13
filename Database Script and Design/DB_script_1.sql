6bd857e394e7d2a2cd2f881d8f0e436ae77c2100
Rafsan Jany <75219480+FahmidaNourin@users.noreply.github.com>
2022-08-07 20:31:20 +0600

    Update DB_script_1.sql

--- Database Script and Design/DB_script_1.sql
+++ Database Script and Design/DB_script_1.sql
@@ -1,5 +1,5 @@
 CREATE TABLE branch( 
-    id INT(10) auto_increment, 
+    id INT(10) NOT NULL, 
     name VARCHAR(20) NOT NULL,
     address VARCHAR(30) NOT NULL, 
     Primary Key(id,name)
@@ -11,7 +11,7 @@ CREATE TABLE book(
     title VARCHAR(15) NOT NULL,
     author VARCHAR(20) NOT NULL, 
     publisher VARCHAR(20) NOT NULL, 
-    cover VARCHAR(20) NOT NULL,
+    cover VARCHAR(20),
     Primary Key(isbn)
 );
 
@@ -33,7 +33,7 @@ CREATE TABLE member(
 );
 
 CREATE TABLE admin(
-    id INT(10) auto_increment,
+    id INT(10) NOT NULL,
     password INT(50) NOT NULL, 
     first_name VARCHAR(15) NOT NULL,
     middle_name VARCHAR(15),
@@ -60,6 +60,7 @@ CREATE TABLE keeps(
     branch_name VARCHAR(20),
     book_isbn INT(30),
     no_of_copies INT(10),
+    PRIMARY KEY (branch_id, branch_name,book_isbn),
     CONSTRAINT fk3 FOREIGN KEY(branch_id, branch_name)
     REFERENCES branch(id, name),
     CONSTRAINT fk4 FOREIGN KEY(book_isbn)
@@ -76,6 +77,7 @@ CREATE TABLE loans(
     borrow_date DATE,
     return_date DATE,
     fee INT(5),
+    PRIMARY KEY (branch_id, branch_name,book_isbn,member_username),
     CONSTRAINT fk5 FOREIGN KEY(branch_id, branch_name)
     REFERENCES branch(id, name),
     CONSTRAINT fk6 FOREIGN KEY(book_isbn)
@@ -90,7 +92,7 @@ CREATE TABLE branch_phone(
     branch_id INT(10),
     branch_name VARCHAR(20),
     phone INT(30),
-    PRIMARY KEY(phone),
+    PRIMARY KEY(phone,branch_id, branch_name),
     CONSTRAINT fk8 FOREIGN KEY(branch_id, branch_name)
     REFERENCES branch(id, name)
 );
