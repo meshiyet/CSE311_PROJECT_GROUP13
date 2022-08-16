0bb998481cd0be6e550c3ba8720554fdbf52fe6c
Mashiyat Nayeem <mashiyat.nayeem@northsouth.edu>
2022-08-17 00:35:44 +0600

    featured books on landing hardcoded _ cover upload

--- landing.php
+++ landing.php
@@ -1,25 +1,30 @@
+<?php
+    include("connection.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="CSS/landing.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>Dhaka Library</title>
     </head>
     <nav>
-        <?php include 'navbar.php';?>
+        <?php include ("navbar.php");?>
     </nav> 
     <body>
         <div class="container">
             <div class="banner">
                     <div class="banner-content">
                         <div class="search">
-                            <input type="text" class="searchTerm" placeholder="Search for books by ISBN, title, or author">
-                            <button href="#" type="submit" class="searchButton">
-                                <i class="fa fa-search"></i>
-                            </button>
+                                <input type="text" class="searchTerm" name="search" placeholder="Search for books by ISBN, title, author or genre">
+                                <button href="#" type="submit" class="searchButton">
+                                    <i class="fa fa-search"></i>
+                                </button>
                         </div>
                     </div>
             </div>
@@ -29,174 +34,101 @@
                     <h1>Featured books</h1>
                     <div class="featured-items">
                         <div class="book-container">
-                            <img class="cover" src="">
-                            <h3 class="title">book 1</h3>
-                            <p class="author">author</p>
+                            <img class="cover" src="images/cover_thebelljar.jpg">
+                            <h3 class="title">The Bell Jar</h3>
+                            <p class="author">Sylvia Plath</p>
                             <br>
-                            <p class="availability">available in: branch 1, branch 2, branch 3</p>
+                            <p class="availability">Available in: </p>
+                            <?php
+                                $sql = "SELECT branch_name FROM keeps WHERE no_of_copies>0 AND book_isbn = '0061148512'";
+                                $branch = mysqli_query($db, $sql);
+                                while($row = $branch->fetch_assoc())
+                                {
+                                    $branchName = $row['branch_name'];
+                                    echo "<div class='information'>
+                                        <p>$branchName</p>
+                                    </div>";
+                                }
+                            ?>
                         </div>
                         <div class="book-container">
-                            <img class="cover" src="">
-                            <h3 class="title">book 2</h3>
-                            <p class="author">author</p>
+                            <img class="cover" src="images/cover_beforethecoffeegetscold.jpg">
+                            <h3 class="title">Before the Coffee Gets Cold</h3>
+                            <p class="author">Toshikazu Kawaguchi</p>
                             <br>
-                            <p class="availability">available in: branch 1, branch 2, branch 3</p>
+                            <p class="availability">Available in: </p>
+                            <?php
+                                $sql = "SELECT branch_name FROM keeps WHERE no_of_copies>0 AND book_isbn = '1529029589'";
+                                $branch = mysqli_query($db, $sql);
+                                while($row = $branch->fetch_assoc())
+                                {
+                                    $branchName = $row['branch_name'];
+                                    echo "<div class='information'>
+                                        <p>$branchName</p>
+                                    </div>";
+                                }
+                            ?>
                         </div>
                         <div class="book-container">
-                            <img class="cover" src="">
-                            <h3 class="title">book 3</h3>
-                            <p class="author">author</p>
+                            <img class="cover" src="images/cover_themidnightlibrary.jpg">
+                            <h3 class="title">The Midnight Library</h3>
+                            <p class="author">Matt Haig</p>
                             <br>
-                            <p class="availability">available in: branch 1, branch 2, branch 3</p>
+                            <p class="availability">Available in: </p>
+                            <?php
+                                $sql = "SELECT branch_name FROM keeps WHERE no_of_copies>0 AND book_isbn = '0525559477'";
+                                $branch = mysqli_query($db, $sql);
+                                while($row = $branch->fetch_assoc())
+                                {
+                                    $branchName = $row['branch_name'];
+                                    echo "<div class='information'>
+                                        <p>$branchName</p>
+                                    </div>";
+                                }
+                            ?>
                         </div>
                         <div class="book-container">
-                            <img class="cover" src="">
-                            <h3 class="title">book 4</h3>
-                            <p class="author">author</p>
+                            <img class="cover" src="images/cover_theparisapartment.jpg">
+                            <h3 class="title">The Paris Apartment</h3>
+                            <p class="author">Lucy Foley</p>
                             <br>
-                            <p class="availability">available in: branch 1, branch 2, branch 3</p>
+                            <p class="availability">Available in: </p>
+                            <?php
+                                $sql = "SELECT branch_name FROM keeps WHERE no_of_copies>0 AND book_isbn = '0063003058'";
+                                $branch = mysqli_query($db, $sql);
+                                while($row = $branch->fetch_assoc())
+                                {
+                                    $branchName = $row['branch_name'];
+                                    echo "<div class='information'>
+                                        <p>$branchName</p>
+                                    </div>";
+                                }
+                            ?>
                         </div>
-                        <!-- <div class="book-container">
-                            <img class="cover" src="">
-                            <h3 class="title">book 5</h3>
-                            <p class="author"></p>
-                            <p class="availability"></p>
-                        </div> -->
+                        <div class="book-container">
+                            <img class="cover" src="images/cover_normalpeople.jpg">
+                            <h3 class="title">Normal People</h3>
+                            <p class="author">Sally Rooney</p>
+                            <br>
+                            <p class="availability">Available in: </p>
+                            <?php
+                                $sql = "SELECT branch_name FROM keeps WHERE no_of_copies>0 AND book_isbn = '1984822187'";
+                                $branch = mysqli_query($db, $sql);
+                                while($row = $branch->fetch_assoc())
+                                {
+                                    $branchName = $row['branch_name'];
+                                    echo "<div class='information'>
+                                        <p>$branchName</p>
+                                    </div>";
+                                }
+                            ?>
+                        </div>
                     </div>
                 </div>      
             </div>
         </div>
     </body>
     <footer>
-        <!-- <?php include 'footer.html';?> -->
+        <?php include ("footer.html");?>
     </footer>
 </html>
-
-<style>
-
-.container{
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    justify-content: center;
-    background-color: lemonchiffon;
-    font-family: 'Roboto', sans-serif;
-    /* min-height: 100vh; */
-}
-
-.banner{
-    margin: 0 10%;
-    padding: 0;
-    height: 15rem;
-    /* width: 100%; */
-    background-color: gray;
-    background-image: url("images/landing_background.jpg");
-    background-size: cover;
-    background-repeat: no-repeat;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-}
-
-.banner-content{
-        background-color: white;
-        border-radius: 10px;
-        height: 50%;
-        width: 50%;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-}
-
-.search {
-  width: 75%;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.searchTerm {
-  width: 100%;
-  border: 2px solid gray;
-  border-right: none;
-  padding: 10px;
-  height: 50px;
-  outline: none;
-  color: black;
-}
-
-.searchTerm:focus{
-  color: black;
-}
-
-.searchButton {
-  width: 50px;
-  height: 50px;
-  border: 1px solid plum;
-  background: plum;
-  text-align: center;
-  color: #fff;
-  cursor: pointer;
-  font-size: 20px;
-}
-.featured{
-    margin: 2rem 10%;
-    justify-content: left;
-}
-
-.featured-content{
-    background-color: white;
-    height: auto;
-}
-
-h1{
-    padding-bottom: 1rem;
-}
-
-.featured-items{
-    /* display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    flex-wrap: nowrap; */
-    height: 20rem;
-    display: grid;
-    grid-template: 1fr / repeat(4, 1fr);
-}
-
-.book-container{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: pink;
-    padding: 0 15%;
-    border: 2px magenta solid;
-    font-family: "Roboto", sans-serif;
-    font-size: 14px;
-}
-
-.cover{
-    height: 13rem;
-    width: 10rem;
-    background-color: grey;
-    margin-bottom: 1rem;
-}
-
-.title{
-    text-align: center;
-}
-
-.author{
-    text-align: center;
-    color: darkslategrey;
-    font-weight: bold;
-    font-style: italic;
-}
-
-.availibility{
-    text-align: center;
-    font-size: xx-small;
-}
-</style>
