af10c948b0ba787815076c5652394cd6d0333f1b
Mashiyat Nayeem <107493183+meshiyet@users.noreply.github.com>
2022-08-30 22:02:30 +0600

    Delete Database Script and Design directory

--- Database Script and Design/featured_keeps.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `keeps` (`branch_name`, `book_isbn`, `no_of_copies`) VALUES
-('Basundhara', '0061148512', 2),
-('Basundhara', '0063003058', 4),
-('Basundhara', '0525559477', 2),
-('Basundhara', '1529029589', 1),
-('Basundhara', '1984822187', 5);
-
